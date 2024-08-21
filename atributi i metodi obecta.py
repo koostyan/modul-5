00cb9a5e791f997f1275e4162b6f7acc64eeb248
koostyan <165835474+koostyan@users.noreply.github.com>
2024-08-21 14:33:51 +0300

    Add files via upload

---
+++ atributi i metodi obecta.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+
+house = House()
+numberOfFloors = 10
+
+for i in range(numberOfFloors):
+    print("Текущий этаж равен ", i)
